de70a7ce689c2b64abca918f0f290a6770a1692f
kaywanbalbas <kaywanbalbas@gmail.com>
2020-06-03 17:01:39 -0700

    Add functionality to play sounds based keyboard input

--- index.js
+++ index.js
@@ -1,53 +1,63 @@
 // number of drum buttons
 var numButtons = document.querySelectorAll(".drum").length;
 
-// adds event listener to all drum buttons
+// detects input from mouse click event
 for (var i = 0; i < numButtons; i++) {
-
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
     var buttonInnerHTML = this.innerHTML;
-
-    // plays corresponding drum audio given user input
-    switch (buttonInnerHTML) {
-      case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case "a":
-        var tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case "s":
-        var tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case "d":
-        var tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      case "j":
-        var snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-
-      case "k":
-        var crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case "l":
-        var kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-
-      default:
-        console.log(buttonInnerHTML);
-        break;
-    }
+    playSound(buttonInnerHTML);
+    console.log(event);
   });
+}
+
+// detects input from keyboard using keydown event
+document.addEventListener("keydown", function(event) {
+  playSound(event.key);
+  console.log(event);
+});
+
+// plays corresponding drum audio given user input
+function playSound(key) {
+
+  switch (key) {
+    case "w":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "a":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "s":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case "d":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case "j":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case "k":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case "l":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default:
+      console.log(buttonInnerHTML);
+      break;
+  }
 
 }
